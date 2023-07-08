885d43c513cfd9b85be3644463ffd36a0ef094c6
ISMIALKHAN17 <ik0105446@gmail.com>
2023-07-08 16:53:29 +0500

    7/8

--- src/app/components/task/task.component.ts
+++ src/app/components/task/task.component.ts
@@ -15,15 +15,23 @@ export class TaskComponent {
   clients:any
   staff:any 
   taskForm:any
+  taskViewData:any
   tasks:any
+  user:any
+  taskView:any = false
+  memos:any
+  memoForm:any
 
   constructor(private req:RequestService , private formBuilder: FormBuilder , private modalService: NgbModal , private datePipe:DatePipe){}
   ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    const user = localStorage.getItem('user')
+    this.user = JSON.parse(user!)
     this.getClients()
     this.getStaff()
     this.getTasks()
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
+
     this.taskForm = this.formBuilder.group({
       name: ['', Validators.required],
       description: ['', Validators.required],
@@ -32,8 +40,15 @@ export class TaskComponent {
       user_id: ['', Validators.required],
       repeatTask: ['select', Validators.required],
       memo: ['', Validators.required],
-      status: ['pending', Validators.required],
-      
+      status: ['Pending', Validators.required],
+      admin_id:[this.user.id, Validators.required],
+    });
+
+    this.memoForm = this.formBuilder.group({
+      memo: ['', Validators.required],
+      task_id: ['', Validators.required],
+      admin_id:[this.user.id, Validators.required],
+      type:['', Validators.required],
     });
     
   }
@@ -54,15 +69,12 @@ export class TaskComponent {
   }
   getTasks(){
     this.loading = true 
-    this.req.post('task/list',true).subscribe((res:any)=>{
+    this.req.post('task/list',{user_id:this.user.id}).subscribe((res:any)=>{
       this.tasks = res.data
       this.loading = false 
       console.log(this.tasks)
     })
   }
-
- 
-
   onSubmit() {
     this.loading = true;
     if (this.taskForm.valid) {
@@ -101,17 +113,21 @@ export class TaskComponent {
     }
   }
   
+  viewTask(task:any) {
+    this.taskViewData = task; 
+    this.getMemos();
+    // Set taskView to the selected task
+  }
 
+ 
+  
   open(modal:any){
     this.modalService.open(modal,{centered:true})
   }
-
   formatDate(date: string): string {
     const formattedDate = this.datePipe.transform(date, 'MM/dd/yyyy');
     return formattedDate || '';
   }
-
-
   deleteTask(id: any) {
     Swal.fire({
       title: 'Are you sure?',
@@ -131,6 +147,55 @@ export class TaskComponent {
         });
       }
     });
+  }
+
+  formatDateWithTime(dateString: string): string {
+    const date = new Date(dateString);
+    const options:any = {
+      month: 'short',
+      day: 'numeric',
+      year: 'numeric',
+      hour: 'numeric',
+      minute: 'numeric',
+      hour12: true
+    };
+    return date.toLocaleString('en-US', options);
+  }
+
+  getMemos(){
+    this.req.post('chat/list',{task_id:this.taskViewData.id}).subscribe((res:any)=>{
+      this.memos = res
+     return this.memos.sort((a:any, b:any) => {
+      const timeA = new Date(a.created_at).getTime();
+      const timeB = new Date(b.created_at).getTime();
+      return timeA - timeB;})
+    })
+  }
+
+  
+  sendMemo() {
+    this.memoForm.patchValue({
+      task_id: this.taskViewData.id,
+      type:this.user.role == 'admin' ? 'admin' :'staff'
+    });
+    if (this.memoForm.valid) {
+      this.req.post('chat', this.memoForm.value).subscribe(
+        (res: any) => {
+          this.memoForm.reset();
+          this.getMemos();
+        },
+        (error: any) => {
+          console.error('Error sending memo:', error);
+          Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Failed to send memo. Please try again later.',
+          });
+        }
+      );
+    } else {
+      this.memoForm.markAllAsTouched();
+    }
   }
   
 }
